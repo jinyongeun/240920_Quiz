5525b85fce8dcb9f982b2424994f429d1e902a47
jinyongeun <163504518+jinyongeun@users.noreply.github.com>
2024-09-20 10:54:02 +0900

    3주차

---
+++ 240920_Quiz.py
@@ -0,0 +1,22 @@
+# 첫번쨰 퀴즈
+word = "Hello, World!"
+print(word[0:5])
+
+# 두번째 퀴즈
+result = (7 + 3) ** 2
+
+# 세번쨰 퀴즈
+A = [1, 2, 3, 4, 5]
+result_list = []
+
+result_list.append(A[4])
+result_list.append(A[3])
+result_list.append(A[2])
+result_list.append(A[1])
+result_list.append(A[0])
+
+print(result_list)
+
+# 네번째 퀴즈
+fruits = {'apple': 3, 'banana': 5, 'cherry': 2}
+print(fruits['banana'])
